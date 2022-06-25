7a5bcb9b8ecaf8f9b99059c26131709546bdd2d6
hope-k <hopekumordzie@gmail.com>
2022-06-25 23:41:47 +0800

    moved mongodb url to .env

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_CONNECTION, {)
+mongoose.connect(process.env.DB_CONNECTION)
 .then((result)=>{console.log('Database connected')})
 .catch((error)=>{console.log('There was an error connecting - '+error.message)})
 
